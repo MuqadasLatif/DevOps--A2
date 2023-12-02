c29bb2f33992d740688ac8881f5cb11093656790
Muqadas <muqadaslatif254@gmail.com>
2023-12-01 23:35:40 -0800

    Added Web Scraper program

---
+++ web_scrap.py
@@ -0,0 +1,24 @@
+# web_scraper.py
+
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_web_page(url):
+    try:
+        # Fetch the HTML content of the web page
+        response = requests.get(url)
+        response.raise_for_status()
+
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Extract clean text from the parsed HTML
+        clean_text = soup.get_text(separator='\n', strip=True)
+
+        return clean_text
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+if __name__ == "__main__":
